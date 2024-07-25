c35596d33b822d552be077d48a37e90bfc762d28
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-26 00:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
-
-
+  // Get all the <strong> elements in the paragraph
+  var boldWords = document.getElementsByTagName("strong");
+  
+  // Loop through all the <strong> elements and set their color to green
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "green";
+  }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  // Get all the <strong> elements in the paragraph
+  var boldWords = document.getElementsByTagName("strong");
+  
+  // Loop through all the <strong> elements and set their color back to black
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "black";
+  }
 }
